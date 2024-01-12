57bc5b9041ffc1cd47dbc770fd54194641b0559a
Rodolfo Henrique <viniciustrajanobs@gmail.com>
2024-01-12 16:36:16 -0300

    refatorando projeto, renomeando mensagens e definindo ações.

--- Screen-Sound/Program.cs
+++ Screen-Sound/Program.cs
@@ -1,4 +1,6 @@
-﻿// utlizamos o padrão camelCase e PascalCase para nomear funções, sendo a primeira letra maiúscula.
+﻿// utilizamos o padrão camelCase e PascalCase para nomear funções, sendo a primeira letra maiúscula.
+// utilizamos o console.ReadLine quando esperamos que a pessoa digite algo na saída.
+// utlizamos o Console.write quando não queremos que pule uma linha.
 string mensagemDeBoasVindas = "Boas vindas ao Screen Sound";
 
 void ExibirMensagemDeBoasVindas()
@@ -20,6 +22,26 @@ void ExibirOpcoesDoMenu()
     Console.WriteLine("Digite 3 para avaliar uma banda");
     Console.WriteLine("Digite 4 para exibir a média de uma banda");
     Console.WriteLine("Digite -1 para sair");
+
+    Console.Write("\nDigite a sua opção: ");
+    string opcaoEscolhida = Console.ReadLine()!; // sinal de exclamação para não trabalhar com valores nulos.
+    int opcaoEscolhidaNumerica = int.Parse(opcaoEscolhida); // utilizamos a função Parse dentro de int para converter string para numéricos.
+
+    switch(opcaoEscolhidaNumerica)
+    {
+        case 1: Console.WriteLine("Você escolheu a opção " + opcaoEscolhida);
+            break;
+        case 2: Console.WriteLine("Você escolheu a opção " + opcaoEscolhida);
+            break;
+        case 3: Console.WriteLine("Você escolheu a opção " + opcaoEscolhida);
+            break;
+        case 4: Console.WriteLine("Você escolheu a opção " + opcaoEscolhida);
+            break;
+        case -1: Console.WriteLine("Bye Bye :) ");
+            break;
+        default: Console.WriteLine("Opção inválida");
+            break;
+    }
 }
 
 ExibirMensagemDeBoasVindas();
